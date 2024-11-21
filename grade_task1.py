ea31c91ea63619b072467e1389e204c39c037fe2
Raoul Grasman <rgrasman@uva.nl>
2024-11-22 00:20:53 +0100

    Update grade_task1.py
    Added a sequence of unit tests

--- grade_task1.py
+++ grade_task1.py
@@ -1,9 +1,35 @@
-import numpy as np
-from tasks import *
-from numpy.testing import assert_array_equal
+import unittest
 
-def test_compute_output_size_1d():
-    input_array = np.array([1, 2, 3, 4, 5])
-    kernel_array = np.array([1, 0, -1])
-    expected_output_length = 3
-    assert compute_output_size_1d(input_array, kernel_array) == expected_output_length
+def tokenize(string: str) -> []:
+    return "".join([x for x in string.lower() if x in '\n\t abcdefghijklmnopqrstuvwxyz0123456789']).split()
+
+class TestTokenizeFunction(unittest.TestCase):
+    
+    def test_basic_sentence(self):
+        self.assertEqual(tokenize("Hello, world!"), ["hello", "world"])
+
+    def test_mixed_case(self):
+        self.assertEqual(tokenize("HeLLo WoRLd"), ["hello", "world"])
+
+    def test_punctuation_removal(self):
+        self.assertEqual(tokenize("This, is a test!"), ["this", "is", "a", "test"])
+
+    def test_extra_spaces(self):
+        self.assertEqual(tokenize("  Lots   of   spaces   "), ["lots", "of", "spaces"])
+
+    def test_empty_string(self):
+        self.assertEqual(tokenize(""), [])
+
+    def test_only_punctuation(self):
+        self.assertEqual(tokenize("!@#$%^&*()"), [])
+
+    def test_single_word(self):
+        self.assertEqual(tokenize("Word"), ["word"])
+
+    def test_special_characters(self):
+        self.assertEqual(tokenize("Hello @#%& world!"), ["hello", "world"])
+
+    def test_newlines_and_tabs(self):
+        self.assertEqual(tokenize("Hello\nworld\tthis\tis\na test"), ["hello", "world", "this", "is", "a", "test"])
+
+unittest.main()
