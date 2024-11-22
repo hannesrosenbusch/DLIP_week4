ca6a7cd4ca851dca3bf5a57487bf59779162f219
Raoul Grasman <rgrasman@uva.nl>
2024-11-22 02:34:43 +0100

    Update grade_task4.py
    added a unit test

--- grade_task4.py
+++ grade_task4.py
@@ -1,9 +1,12 @@
-import numpy as np
 from tasks import *
-from numpy.testing import assert_array_equal
 
-def test_convolute_2d():
-    input_matrix = np.array([[2, 2, 3], [4, 5, 6], [7, 8, 10]])
-    kernel_matrix = np.array([[1, 0], [0, -1]])
-    expected_output = np.array([[-3, -4], [-4, -5]])
-    assert_array_equal(convolute_2d(input_matrix, kernel_matrix), expected_output)
+def test_token_counts():
+    text = """The quick brown fox jumps over the lazy dog. The fox and the dog play together. 
+              The fox chases the dog, but the dog runs quickly. The fox is fast, and the dog escapes."""
+    expected = {'the': 9, 'quick': 1, 'brown': 1, 'fox': 4, 'jumps': 1, 'over': 1, 'lazy': 1, 'dog': 5, 
+                'and': 2, 'play': 1, 'together': 1, 'chases': 1, 'but': 1, 'runs': 1, 'quickly': 1, 
+                'is': 1, 'fast': 1, 'escapes': 1}
+    obtained = token_counts(text)
+    assert type(obtained) == dict, "expected return type 'dict'"
+    assert set(obtained.keys()) == set(expected.keys()), "unexpected keys in dict"
+    assert all(obtained[key] == expected[key] for key in expected), "unexpected counts"
