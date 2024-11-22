43f7300b4ecd4756531b0b7915231374a26010e3
Raoul Grasman <rgrasman@uva.nl>
2024-11-22 03:50:44 +0100

    Update grade_task3.py
    Added test for vocabulary map builder

--- grade_task3.py
+++ grade_task3.py
@@ -1,10 +1,5 @@
-import numpy as np
 from tasks import *
-from numpy.testing import assert_array_equal
 
-
-def test_compute_output_size_2d():
-    input_matrix = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-    kernel_matrix = np.array([[1, 0], [0, -1]])
-    expected_output_size = (2, 2)
-    assert compute_output_size_2d(input_matrix, kernel_matrix) == expected_output_size
+def test_vocabulary_builder():
+    t2i, i2t = make_vocabulary_map([text])
+    assert all(i2t[t2i[tok]] == tok for tok in t2i), "something wrong with translation dicts"
